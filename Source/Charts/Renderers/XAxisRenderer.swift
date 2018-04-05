2b2f5adb9fcdab71ac50bad7e2900fd22f3665ee
Andrii T <andriternovyi@gmail.com>
2018-04-05 14:47:07 +0300

    Draw even and odd bars in different colors

--- Source/Charts/Renderers/XAxisRenderer.swift
+++ Source/Charts/Renderers/XAxisRenderer.swift
@@ -392,6 +392,8 @@ open class XAxisRenderer: NSObject, AxisRenderer
             return
         }
         
+        context.setFillColor(axis.evenBarsColor.cgColor)
+        
         context.saveGState()
         defer { context.restoreGState() }
         context.clip(to: self.gridClippingRect)
@@ -416,11 +418,28 @@ open class XAxisRenderer: NSObject, AxisRenderer
         
         let entries = axis.entries
         
+        var itter = 0
         for i in stride(from: 0, to: entries.count, by: 1)
         {
             position.x = CGFloat(entries[i])
             position.y = position.x
             position = position.applying(valueToPixelMatrix)
+            
+            itter += 1
+            if (itter % 2 == 0) {
+                let x =  CGFloat(entries[i-1])
+                var prevPosition = CGPoint(x: x, y: 0.0)
+                prevPosition = prevPosition.applying(valueToPixelMatrix)
+                let width = position.x - prevPosition.x
+                let height = viewPortHandler.contentBottom - viewPortHandler.contentTop
+                let rect = CGRect.init(x: position.x, y: viewPortHandler.contentTop, width: width, height: height)
+                context.fill(rect)
+                
+                if itter == 2 {
+                    let rect = CGRect.init(x: position.x - 2 * width, y: viewPortHandler.contentTop, width: width, height: height)
+                    context.fill(rect)
+                }
+            }
             
             drawGridLine(context: context, x: position.x, y: position.y)
         }
